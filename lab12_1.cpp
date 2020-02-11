164a5bd54d717f1f779f595aebe1db2347b17159
pondcmu <poncmu@gmail.com>
2020-02-11 21:34:07 +0700

    Pond

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -16,5 +16,27 @@ int main()
     cout << "\nMin = " << B[3];
     return 0;
 }
-
+void stat(const double A[],int N,double B[])
+{
+    double sum1=0;
+    double sum2=0;
+    for(int i=0;i<N;i++)
+    {
+        sum1=sum1+A[i];
+    }
+    for(int i=0;i<N;i++)
+    {
+        sum2=sum2+pow(A[i]-sum1/N,2);
+    }
+    double max=A[0],min=A[0];
+    for(int i=0;i<N;i++)
+    {
+        if(A[i]>max) max=A[i];
+        if(A[i]<min) min=A[i];
+    }
+    B[0]=sum1/N;
+    B[1]=pow(sum2/N,0.5);
+    B[2]=max;
+    B[3]=min;
+}
 //Write definition of stat() here 
--- lab12_2.cpp
+++ lab12_2.cpp
@@ -21,5 +21,37 @@ int main()
     }while(s != 0 && x != 0 && y != 0 );
              return 0;    
 }
-
+void updateImage(bool image[N][M],int s,int x,int y)
+{
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<M;j++)
+        {
+            if(pow(pow(x-j,2)+pow(y-i,2),0.5)<=s-1)
+            image[i][j]=1;
+            else
+            {
+                image[i][j]=0;
+            }
+            
+        }
+    }
+}
+void showImage(const bool image[N][M])
+{
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<M;j++)
+        {
+            if(image[i][j]==1)
+            cout<<"*";
+            else
+            {
+                cout<<" ";
+            }
+            
+        }
+        cout<<endl;
+    }
+}
 // Write definition of updateImage() and showImage() here
